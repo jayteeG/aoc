541b56cfb1bee8af8fe6db86b1d9126439122cfa
jayteeG <56824070+jayteeG@users.noreply.github.com>
2021-12-03 14:52:20 +0000

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,11 +1,11 @@
 import pandas as pd
 # q1
-((int("".join(str(i) for i in (pd.read_csv('input.txt',sep=' ',header=None,dtype='str')[0].str.split("",
+((int("".join(str(i) for i in (pd.read_csv('day3.txt',sep=' ',header=None,dtype='str')[0].str.split("",
                                                                       expand = True)[range(1,
-                                                                                               len(pd.read_csv('input.txt',sep=' ',
-                                                                                                               header=None,dtype='str')[0].str.split("",expand = True).columns)-1)].applymap(int).sum()>len(pd.read_csv('input.txt',sep=' ',header=None))/2).astype(int).values),2))
+                                                                                               len(pd.read_csv('day3.txt',sep=' ',
+                                                                                                               header=None,dtype='str')[0].str.split("",expand = True).columns)-1)].applymap(int).sum()>len(pd.read_csv('day3.txt',sep=' ',header=None))/2).astype(int).values),2))
 *
-(int("".join(str(i) for i in (pd.read_csv('input.txt',sep=' ',header=None,dtype='str')[0].str.split("",
+(int("".join(str(i) for i in (pd.read_csv('day3.txt',sep=' ',header=None,dtype='str')[0].str.split("",
                                                                       expand = True)[range(1,
-                                                                                               len(pd.read_csv('input.txt',sep=' ',
-                                                                                                               header=None,dtype='str')[0].str.split("",expand = True).columns)-1)].applymap(int).sum()<len(pd.read_csv('input.txt',sep=' ',header=None))/2).astype(int).values),2)))
+                                                                                               len(pd.read_csv('day3.txt',sep=' ',
+                                                                                                               header=None,dtype='str')[0].str.split("",expand = True).columns)-1)].applymap(int).sum()<len(pd.read_csv('day3.txt',sep=' ',header=None))/2).astype(int).values),2)))
