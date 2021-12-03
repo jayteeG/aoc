bd01e461cf10a92ac2e7008e666f951011607e35
jayteeG <56824070+jayteeG@users.noreply.github.com>
2021-12-03 13:56:30 +0000

    add day3 q1

---
+++ day3.py
@@ -0,0 +1,11 @@
+import pandas as pd
+# q1
+((int("".join(str(i) for i in (pd.read_csv('input.txt',sep=' ',header=None,dtype='str')[0].str.split("",
+                                                                      expand = True)[range(1,
+                                                                                               len(pd.read_csv('input.txt',sep=' ',
+                                                                                                               header=None,dtype='str')[0].str.split("",expand = True).columns)-1)].applymap(int).sum()>len(pd.read_csv('input.txt',sep=' ',header=None))/2).astype(int).values),2))
+*
+(int("".join(str(i) for i in (pd.read_csv('input.txt',sep=' ',header=None,dtype='str')[0].str.split("",
+                                                                      expand = True)[range(1,
+                                                                                               len(pd.read_csv('input.txt',sep=' ',
+                                                                                                               header=None,dtype='str')[0].str.split("",expand = True).columns)-1)].applymap(int).sum()<len(pd.read_csv('input.txt',sep=' ',header=None))/2).astype(int).values),2)))
